1cd933360005207bbebd17332368011bdbdc0afc
virgil <virgilx13@gmail.com>
2017-11-20 19:05:42 +0200

    chat between peers working. TODO handle errors, more versatile connect

--- peer.py
+++ peer.py
@@ -16,32 +16,45 @@ class peer(object):
     """docstring for peer."""
     def startServer(self):
         self.server.start()
+        connected = False
+        pass
+    def killServer(self):
+        self.server.kill()
         pass
     def connect(self, host, port):
         self.client.connect(host, port)
+        self.connected = True
         pass
     def makeRequest(self, request):
-        self.client.makeRequest(request)
-        pass
-    def __init__(self, serverHost, serverPort, requestHandler, nbReq):
+        if (self.connected == False):
+            self.connect(self.destHost, destPort)
+        return self.client.makeRequest(request)
+
+    def __init__(self, destHost, serverPort, requestHandler, nbReq):
         super(peer, self).__init__()
         self.server = serverSocket(serverPort, requestHandler, nbReq)
+        self.destHost = destHost
         self.client = clientSocket()
+        self.connected = False
 
 
 def requestHandler(addr, req):
-    print("SHE SAID > %s" % (str(req)))
-    return ("(ac) if you say so")
+    print("I GOT AS REQUEST > %s\n" % (str(req)))
+    return ("RESPONSE")
 
 
 if (len(sys.argv) > 2):
     hostPort = int(sys.argv[1])
     destPort = int(sys.argv[2])
-    peer = peer(socket.gethostname(), hostPort, requestHandler, 1)
+    peer = peer(socket.gethostname(), hostPort, requestHandler, 5)
     peer.startServer()
-    while True:
+    stop = 0
+    while stop != 1:
         text = raw_input("I SAY > ")
-        peer.connect(socket.gethostname(), destPort)
-        print ("SHE SAID > %s" % (peer.makeRequest(text)))
+        if (text == "stop"):
+            stop = 1
+            peer.killServer()
+        else:
+            print ("I GOT AS RESPONSE > %s\n" % (peer.makeRequest(text)))
 else:
     print (sysConfig["usage"])
--- scSocket.py
+++ scSocket.py
@@ -8,34 +8,39 @@ class serverSocket(threading.Thread):
     """docstring for serverSocket."""
 
     def bind(self):
+        print ("[serverSocket] bind")
         self.socket.bind((self.host, self.port))
         pass
 
     def listen(self, reqNb):
+        print("[serverSocket] listen")
         self.socket.listen(reqNb)
         pass
 
     def accept(self, requestHandler):
-        clientsocket,addr = self.socket.accept()
-        req = clientsocket.recv(1024)
-        clientsocket.send
-        clientsocket.send(requestHandler(addr, req))
-        clientsocket.close()
+        print("[serverSocket] accept")
+        req = self.clientsocket.recv(1024)
+        self.clientsocket.send(requestHandler(self.clientaddr, req))
+        self.clientsocket.close()
         pass
 
     def run(self):
         self.listen(self.reqNb)
         self.running = True
         print("[serverSocket] started running")
+        self.clientsocket,self.clientaddr = self.socket.accept()
         while self.running == True:
             self.accept(self.requestHandler)
+            time.sleep(0)
         pass
     def kill(self):
         self.running = False
+        print("[serverSocket] killed")
         pass
     def __init__(self, port, requestHandler, reqNb):
         threading.Thread.__init__(self)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.host = socket.gethostname()
         self.port = port
         self.requestHandler = requestHandler
@@ -47,15 +52,17 @@ class serverSocket(threading.Thread):
 class clientSocket(object):
     """docstring for clientSocket."""
     def connect(self, host, port):
-        self.socket.connect((host, port))
-        print("[clientSocket] connected to host: %s\tport: %s" % (str(host), str(port)) )
+        errno = self.socket.connect_ex((host, port))
+        print("[clientSocket] connected to host: %s\tport: %s\terrno:%s" % (str(host), str(port), str(errno)) )
         pass
 
     def makeRequest(self, request):
+        print("[clientSocket] makeRequest")
         self.socket.send(request)
         return self.socket.recv(1024)
 
     def close(self):
+        print("[clientSocket] close")
         self.socket.close()
         pass
 
